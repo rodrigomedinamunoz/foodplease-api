230d12fc54c71130259406ecb4561f55059a16e5
Rodrigo Medina Muñoz <r_medina@inacap.cl>
2025-12-19 10:56:34 -0300

    Fix CORS Flutter Web

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+# CORS explícito para todos los endpoints
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 USERS = {
     "admin": "1234",
@@ -11,8 +15,13 @@ USERS = {
 def home():
     return jsonify({"status": "FoodPlease API en ejecución..."})
 
-@app.route("/login", methods=["POST"])
+
+@app.route("/login", methods=["POST", "OPTIONS"])
 def login():
+    # RESPUESTA AL PREFLIGHT
+    if request.method == "OPTIONS":
+        return jsonify({"status": "ok"}), 200
+
     data = request.get_json()
 
     username = data.get("username")
@@ -29,3 +38,15 @@ def login():
             "message": "Credenciales incorrectas"
         }), 401
 
+
+@app.route("/platos", methods=["GET"])
+def platos():
+    return jsonify([
+        {"id": 1, "name": "Cazuela de Pollo", "price": 5990},
+        {"id": 2, "name": "Empanada de Pino", "price": 1990},
+        {"id": 3, "name": "Pastel de Choclo", "price": 7990},
+        {"id": 4, "name": "Lomo a lo Pobre", "price": 8990},
+        {"id": 5, "name": "Completo Italiano", "price": 1990},
+        {"id": 6, "name": "Bebida 350 cc", "price": 1490},
+        {"id": 7, "name": "Papas Fritas Familiar", "price": 4990},
+    ])
